4b077de79e9503c2f1503a4e861be0a34eec0839
nugliar <hashan.hall@gmail.com>
2021-06-12 14:37:13 -0500

    bugfixes

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Display } from './Display'
 import { Buttons } from './Buttons'
 import { FormulaDisplay } from './FormulaDisplay'
+import githubIcon from './img/GitHub-Mark-64px.png'
 
 function App() {
   return (
@@ -11,6 +12,9 @@ function App() {
       <div className='outer-container'>
         <div className='calculator'>
           <div className='description'>
+            <a className='link' href='https://github.com/nugliar/calculator'>
+              <img src={githubIcon} alt='git' />
+            </a>
             <p>Formula Calculator</p>
           </div>
           <FormulaDisplay />
--- src/logicSlice.js
+++ src/logicSlice.js
@@ -13,6 +13,9 @@ const EQUAL = '='
 
 const PRECISION = 15
 
+const ERROR_SYNTAX = 'SYNTAX ERROR'
+const ERROR_DIVISION_BY_ZERO = 'DIVISION BY ZERO'
+
 const newOperand = () => {
   return {
     type: OPERAND,
@@ -30,16 +33,102 @@ const newOperator = () => {
   }
 }
 
+
+// For NaN values a special big-integer operand is used.
+const NAN_OPERAND = {...newOperand(), coefficient: null}
+
+const calculatePrimitive = (operand2, operator, operand1) => {
+
+  // Evaluate primitive expression
+
+  if (operator === undefined) {
+    return NAN_OPERAND
+  }
+  if (operand2 === undefined || operand2.coefficient === null) {
+    return NAN_OPERAND
+  }
+  if (operand1 === undefined) {
+
+    switch(operator.symbol) {
+      case MINUS:
+        return {...operand2, isNegative: !operand2.isNegative}
+      case PLUS:
+        return operand2
+      default:
+        return NAN_OPERAND
+    }
+  }
+  if (operand1.coefficient === null) {
+    return NAN_OPERAND
+  }
+
+  const sign1 = operand1.isNegative ? -1 : 1
+  const sign2 = operand2.isNegative ? -1 : 1
+  const c1 = operand1.coefficient * BigInt(sign1)
+  const c2 = operand2.coefficient * BigInt(sign2)
+  const e2 = operand2.exponent
+  const e1 = operand1.exponent
+
+  const result = newOperand()
+
+  let coeff = 0n
+  let expo = 0
+
+  switch(operator.symbol) {
+    case MULTIPLY:
+      coeff = c1 * c2
+      expo = e1 + e2
+      break
+    case DIVIDE:
+      if (c2 === BigInt(0)) {
+        throw(new Error(ERROR_DIVISION_BY_ZERO))
+      }
+      expo = -PRECISION
+      coeff = (c1 * 10n ** BigInt(-expo)) / c2
+      expo += e1 - e2
+      break
+    case PLUS:
+      expo = Math.min(e1, e2);
+      coeff = c1 * 10n ** BigInt(-expo + e1) +
+        c2 * 10n ** BigInt(-expo + e2)
+      break
+    case MINUS:
+      expo = Math.min(e1, e2)
+      coeff = c1 * 10n ** BigInt(-expo + e1) -
+        c2 * 10n ** BigInt(-expo + e2)
+      break
+    default:
+      break
+  }
+
+  // Clamp trailing zeroes
+  while (expo < 0 && coeff % 10n === 0n) {
+    coeff /= 10n
+    expo++
+  }
+  result.coefficient = coeff > 0n ? coeff : -coeff
+  result.exponent = expo
+  result.isNegative = coeff < 0n
+  result.isRational = expo < 0
+
+  return result
+}
+
 const displayOperand = (operand) => {
+
+  if (!operand || operand.coefficient === null) {
+    return ('NaN')
+  }
+
   const sign = operand.isNegative ? -1 : 1
   const coeff = operand.coefficient * BigInt(sign)
   const expo = operand.exponent
   let display = coeff.toString()
 
+  // Handle floats that equal to zero (i.e 0.00000)
   if (display.length <= -expo) {
     display = '0'.repeat(-expo - display.length + 1).concat(display)
   }
-
   if (expo || operand.isRational) {
     return display
       .slice(0, display.length - -expo)
@@ -53,8 +142,6 @@ const displayOperand = (operand) => {
 const displayFormula = (expression) => {
   const expr = [...expression]
 
-  expr.reverse()
-
   return expr.map(item => {
     switch(item.type) {
       case OPERAND:
@@ -67,9 +154,21 @@ const displayFormula = (expression) => {
   }).join(' ')
 }
 
+// Extract big-integer array from redux state
+// to avoid "non-serializable type" redux error
+
+let bigIntExpression = []
+
+// Array is a sequence of operands and operator which form
+// a mathematical expression. Numbers are represented
+// as a big-integers in the following form:
+
+// NUMBER = COEFFICIENT * 10 ^ EXPONENT
+
+// This form allows big-integer rational numbers
+
 const initialState = {
-  expression: [newOperand()],
-  lastInput: OPERAND,
+  lastInput: '',
   isNegative: false,
   display: '',
   displayFormula: ''
@@ -82,60 +181,76 @@ const logicSlice = createSlice({
 
     allClear: (state) => {
       Object.assign(state, initialState)
+      bigIntExpression = []
     },
 
     addOperandDigit: (state, action) => {
+
+      // Add digit to operand on top ofthe stack, if any.
+      // Create new operand if needed.
+
       const digit = action.payload
       let operand = newOperand()
 
       if (state.lastInput === EQUALS) {
-        state.expression = []
+        bigIntExpression = []
         state.isNegative = false
       }
       if (state.lastInput === OPERAND) {
-        operand = state.expression.shift()
+        operand = bigIntExpression.pop()
       }
 
+      // Add digit only if operand coefficient is within precision limits
       if (operand.coefficient <= 10n ** BigInt(PRECISION) - 1n) {
         operand.coefficient = 10n * operand.coefficient + BigInt(digit)
         operand.exponent -= operand.isRational ? 1 : 0
         operand.isNegative = state.isNegative
       }
 
-      state.expression.unshift(operand)
+      bigIntExpression.push(operand)
       state.display = displayOperand(operand)
-      state.displayFormula = displayFormula(state.expression)
+      state.displayFormula = displayFormula(bigIntExpression)
       state.lastInput = OPERAND
     },
 
     addFraction: (state) => {
+
+      // Set isRational flag for operand on top ofthe stack, if any.
+      // Create new operand if needed.
+
       let operand = newOperand()
-      let displayAdd = ''
 
       if (state.lastInput === EQUALS) {
-        state.expression = []
+        bigIntExpression = []
         state.isNegative = false
       }
       if (state.lastInput === OPERAND) {
-        operand = state.expression.shift()
+        operand = bigIntExpression.pop()
       }
       if (!operand.isRational) {
         operand.isRational = true
       }
-      state.expression.unshift(operand)
+      bigIntExpression.push(operand)
       state.display = displayOperand(operand)
-      state.displayFormula = displayFormula(state.expression)
+      state.displayFormula = displayFormula(bigIntExpression)
       state.lastInput = OPERAND
     },
 
     addOperator: (state, action) => {
+
+      // If top of the stack is operator, then replace it
+      // with current operator, otherwise add new operator on to
+      // the stack. If current operator is minus and
+      // top of the stack is operator, then treat minus operator as
+      // a negative sign.
+
       let operator = newOperator()
       let symbol = action.payload
       let isNegative = false
       let formulaAdd = ''
 
       if (state.lastInput === OPERATOR) {
-        operator = state.expression.shift()
+        operator = bigIntExpression.pop()
         if (symbol === MINUS) {
           isNegative = symbol === MINUS
           symbol = operator.symbol
@@ -144,107 +259,113 @@ const logicSlice = createSlice({
       }
       operator.symbol = symbol
 
-      state.expression.unshift(operator)
+      bigIntExpression.push(operator)
       state.isNegative = isNegative
       state.display = isNegative ? MINUS : operator.symbol
-      state.displayFormula = displayFormula(state.expression) + formulaAdd
+      state.displayFormula = displayFormula(bigIntExpression) + formulaAdd
       state.lastInput = OPERATOR
     },
 
     calculateExpression: (state, action) => {
-      const expression = [...state.expression]
-      const formula = displayFormula(expression)
-      const nextExpression = []
-      const firstOrder = {
-        [MULTIPLY]: true,
-        [DIVIDE]: true,
-        [PLUS]: false,
-        [MINUS]: false,
-        [EQUAL]: false
+
+      // Convert expression to postfix form and calculate
+
+      // If last item in expression is operator, then remove it
+      if (bigIntExpression.length > 0) {
+        const lastItem = bigIntExpression[bigIntExpression.length - 1]
+        if (lastItem.type === OPERATOR) {
+          bigIntExpression.pop()
+        }
+      }
+      // Stop if nothing to calculate
+      if (bigIntExpression.length === 0) {
+        Object.assign(state, initialState)
+        return
       }
 
-      const calculate = (operator, operand1, operand2) => {
-        const sign1 = operand1.isNegative ? -1 : 1
-        const sign2 = operand2.isNegative ? -1 : 1
-        const c1 = operand1.coefficient * BigInt(sign1)
-        const c2 = operand2.coefficient * BigInt(sign2)
-        const e2 = operand2.exponent
-        const e1 = operand1.exponent
+      state.displayFormula = displayFormula(bigIntExpression)
 
-        const result = newOperand()
-
-        let coeff = 0n
-        let expo = 0
-
-        switch(operator.symbol) {
-          case MULTIPLY:
-            coeff = c1 * c2
-            expo = e1 + e2
-            break
-
-          case DIVIDE:
-            expo = -PRECISION
-            coeff = (c1 * 10n ** BigInt(-expo)) / c2
-            expo += e1 - e2
-            break
-
-          case PLUS:
-            expo = Math.min(e1, e2);
-            coeff = c1 * 10n ** BigInt(-expo + e1) +
-              c2 * 10n ** BigInt(-expo + e2)
-            break
-
-          case MINUS:
-            expo = Math.min(e1, e2)
-            coeff = c1 * 10n ** BigInt(-expo + e1) -
-              c2 * 10n ** BigInt(-expo + e2)
-            break
-
-          default:
-            break
-        }
-
-        while (expo < 0 && coeff % 10n === 0n) {
-          coeff /= 10n
-          expo++
-        }
-
-        result.coefficient = coeff > 0n ? coeff : -coeff
-        result.exponent = expo
-        result.isNegative = coeff < 0n
-        result.isRational = expo < 0
-
-        return result
+      // Precedense of operators
+      const order = {
+        [MULTIPLY]: 2,
+        [DIVIDE]: 2,
+        [PLUS]: 1,
+        [MINUS]: 1,
+        [EQUAL]: 1
       }
 
-      while (expression.length > 0) {
-        const operand1 = expression.pop()
-        const operator = expression.pop()
-        const operand2 = expression.pop()
+      // Convert to postfix form (operator follows operands)
+      let postfixExpr = []
+      let operatorStack = []
+      let prevOperator = undefined
 
-        if (!operand2) {
-          nextExpression.unshift(operand1)
-        } else if (firstOrder[operator.operator]) {
-          expression.push(calculate(operator, operand1, operand2))
+      if (bigIntExpression[0].type === OPERAND) {
+        postfixExpr.unshift(bigIntExpression.shift())
+      }
+      prevOperator = bigIntExpression[0]
+
+      while (bigIntExpression.length > 0) {
+        const item = bigIntExpression.shift()
+
+        if (item.type === OPERATOR) {
+          operatorStack.unshift(item)
+
+        } else if (item.type === OPERAND) {
+
+          const operator = operatorStack[0]
+
+          if (prevOperator) {
+            if (order[prevOperator.symbol] < order[operator.symbol]) {
+              operatorStack.unshift(postfixExpr.shift())
+            }
+          }
+          postfixExpr.unshift(...operatorStack, item)
+          prevOperator = operatorStack[0]
+          operatorStack = []
+        }
+      }
+
+      if (operatorStack.length > 0) {
+        Object.assign(state, initialState)
+        state.display = ERROR_SYNTAX
+        return
+      }
+
+      let operandStack = []
+
+      // Evaluate postfix expression
+      while (postfixExpr.length > 0) {
+        const item = postfixExpr.pop()
+
+        if (item.type === OPERAND) {
+          operandStack.push(item)
         } else {
-          expression.push(operand2)
-          nextExpression.unshift(operator, operand1)
+          try {
+            operandStack.push(
+              calculatePrimitive(
+                operandStack.pop(),
+                item,
+                operandStack.pop()
+              )
+            )
+          } catch(error) {
+            Object.assign(state, initialState)
+            state.display = error.message
+            return
+          }
         }
       }
 
-      while (nextExpression.length > 1) {
-        const operand1 = nextExpression.pop()
-        const operator = nextExpression.pop()
-        const operand2 = nextExpression.pop()
-
-        nextExpression.push(calculate(operator, operand1, operand2))
+      // Display result and update expression stack
+      if (operandStack.length === 1) {
+        state.display = displayOperand(...operandStack)
+        state.displayFormula += ` = ${state.display}`
+        bigIntExpression = [...operandStack]
+      } else {
+        Object.assign(state, initialState)
+        state.display = ERROR_SYNTAX
       }
 
-      const [ result ] = nextExpression
-
-      state.expression = nextExpression
-      state.display = displayOperand(result)
-      state.displayFormula = formula + ` = ${state.display}`
       state.lastInput = EQUALS
     }
   }
